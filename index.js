c4612c0e62554ebdd1ce73c1d2f8bae5b5e037af
Lingga <lingga@xcidic.com>
2019-02-11 10:55:29 +0700

    feat(homepage): edit comment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Moment = require('moment')
 
 // This file is edited in feature/homepage/master
 
+// This file is edited in feature/homepage/edit-comment
+
 exports.handler = function (callback) {
   const response = `[${Moment()}] Hello world!`
 
